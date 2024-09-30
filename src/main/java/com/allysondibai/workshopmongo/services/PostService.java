ef868a8ae99e66e181c9243d09b0178c2709b9ac
Allyson Dibai <allysondibai@outlook.com>
2024-09-30 15:15:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/allysondibai/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.allysondibai.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allysondibai.workshopmongo.domain.Post;
+import com.allysondibai.workshopmongo.repository.PostRepository;
+import com.allysondibai.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+}
