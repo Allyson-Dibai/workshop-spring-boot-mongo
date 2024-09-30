d3abbb2874b31e29845ca12af47c537c5a923edd
Allyson Dibai <allysondibai@outlook.com>
2024-09-30 14:31:57 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/allysondibai/workshopmongo/resources/UrerResource.java
+++ src/main/java/com/allysondibai/workshopmongo/resources/UrerResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.allysondibai.workshopmongo.domain.Post;
 import com.allysondibai.workshopmongo.domain.User;
 import com.allysondibai.workshopmongo.dto.UserDTO;
 import com.allysondibai.workshopmongo.services.UserService;
@@ -60,6 +61,12 @@ public class UrerResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
  
